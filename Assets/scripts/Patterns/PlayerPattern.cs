6df75346fa8922563b64bc327853263a60320ba8
Ting Xiao <tigerting98@gmail.com>
2020-05-29 21:52:20 +0800

    Swap water and earth bullet patterns

--- Assets/scripts/Patterns/PlayerPattern.cs
+++ Assets/scripts/Patterns/PlayerPattern.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerPattern : MonoBehaviour
 {
-    public static void WaterMode(Bullet bullet, Player player)
+    public static void EarthMode(Bullet bullet, Player player)
     {
         float spread = player.isFocus ? 5 : 15;
          Patterns.ShootStraight(bullet, player.transform.position, 90, player.bulletSpeed);
@@ -15,7 +15,7 @@ public class PlayerPattern : MonoBehaviour
         
     }
 
-    public static void EarthMode(Bullet bullet, Player player)
+    public static void WaterMode(Bullet bullet, Player player)
     {
         Vector3 spread = player.isFocus ? new Vector2(0.3f, 0) : new Vector2(1, 0);
         Patterns.ShootStraight(bullet, player.transform.position, 90, player.bulletSpeed);
