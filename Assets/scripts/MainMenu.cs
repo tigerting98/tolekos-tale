688cafe57cad451402198d878c2870f119d26f4f
Ting Xiao <tigerting98@gmail.com>
2020-05-30 23:02:13 +0800

    Change Main Menu UI
    Add pointer to currently selected menu item
    Remove coloured buttons

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -7,21 +7,43 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject instructionMenu;
     public GameObject instructionButton, startButton, instructionBackButton;
+    public GameObject startPointer, instructionPointer, quitPointer;
     void Awake()
     {
         instructionMenu.SetActive(false);
-        
+        instructionPointer.SetActive(false);
+        quitPointer.SetActive(false);
         
     }
     private void Start()
     {
         EventSystem.current.SetSelectedGameObject(null);
+        startPointer.SetActive(true);
         EventSystem.current.SetSelectedGameObject(startButton);
     }
 
 
     void Update()
     {
+        GameObject currentSelection = EventSystem.current.currentSelectedGameObject;
+        if (currentSelection == startButton) 
+        {
+            quitPointer.SetActive(false);
+            instructionPointer.SetActive(false);
+            startPointer.SetActive(true);
+        } 
+        else if (currentSelection == instructionButton || currentSelection == instructionBackButton)
+        {
+            quitPointer.SetActive(false);
+            startPointer.SetActive(false);
+            instructionPointer.SetActive(true);
+        } 
+        else
+        {
+            startPointer.SetActive(false);
+            instructionPointer.SetActive(false);
+            quitPointer.SetActive(true);
+        }
         
     }
     public void OpenInstructionMenu() {
