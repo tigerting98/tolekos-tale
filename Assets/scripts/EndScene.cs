a16e1002e72801535eac3ef9ac02a41d59a7e00f
pooty3 <aloysiuslim.dw@gmail.com>
2020-05-30 21:58:47 +0800

    Changed background and end

--- Assets/scripts/EndScene.cs
+++ Assets/scripts/EndScene.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 public class EndScene : MonoBehaviour
 {
     [SerializeField] Text endText;
+    [SerializeField] Color defeat, victory;
     private void Awake()
     {
         endText.text = GameManager.victory ? "Victory!!" : "YOU DIED";
+        endText.color = GameManager.victory ? victory : defeat;
     }
 }
