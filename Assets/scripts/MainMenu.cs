08d5176cd35713436a86e31cc334957c9c477b84
Ting Xiao <tigerting98@gmail.com>
2020-05-30 23:51:35 +0800

    Change Main Menu UI and fix minor bugs
    Fix issue where clicking on the game screen away from buttons in the main menu caused keyboard controls for the menu to stop working.
    Fix issue where the user could select other buttons while the instructions scene was active.

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -7,7 +7,9 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject instructionMenu;
     public GameObject instructionButton, startButton, instructionBackButton;
-    public GameObject startPointer, instructionPointer, quitPointer;
+    public GameObject startPointer, instructionPointer, instructionBackPointer, quitPointer;
+
+    private GameObject previousSelection;
     void Awake()
     {
         instructionMenu.SetActive(false);
@@ -20,32 +22,64 @@ public class MainMenu : MonoBehaviour
         EventSystem.current.SetSelectedGameObject(null);
         startPointer.SetActive(true);
         EventSystem.current.SetSelectedGameObject(startButton);
+        previousSelection = startButton;
     }
 
 
     void Update()
     {
         GameObject currentSelection = EventSystem.current.currentSelectedGameObject;
-        if (currentSelection == startButton) 
-        {
-            quitPointer.SetActive(false);
-            instructionPointer.SetActive(false);
-            startPointer.SetActive(true);
-        } 
-        else if (currentSelection == instructionButton || currentSelection == instructionBackButton)
-        {
+        if (currentSelection != previousSelection && currentSelection != null) { 
+            if (currentSelection == startButton) 
+            {
+                instructionBackPointer.SetActive(false);
+                quitPointer.SetActive(false);
+                instructionPointer.SetActive(false);
+                startPointer.SetActive(true);
+            } 
+            else if (currentSelection == instructionButton)
+            {
+                instructionBackPointer.SetActive(false);
+                quitPointer.SetActive(false);
+                startPointer.SetActive(false);
+                instructionPointer.SetActive(true);
+            } 
+            else if (currentSelection == instructionBackButton) 
+            {
+                quitPointer.SetActive(false);
+                startPointer.SetActive(false);
+                instructionPointer.SetActive(false);
+                instructionBackPointer.SetActive(true);
+            }
+            else
+            {
+                instructionBackPointer.SetActive(false);
+                startPointer.SetActive(false);
+                instructionPointer.SetActive(false);
+                quitPointer.SetActive(true);
+            }
+            previousSelection = currentSelection;
+        } else if (currentSelection == null) {
             quitPointer.SetActive(false);
             startPointer.SetActive(false);
-            instructionPointer.SetActive(true);
-        } 
-        else
-        {
-            startPointer.SetActive(false);
             instructionPointer.SetActive(false);
-            quitPointer.SetActive(true);
+            instructionBackPointer.SetActive(false);
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow)) 
+            {
+                if (instructionMenu.activeInHierarchy) 
+                {
+                    EventSystem.current.SetSelectedGameObject(instructionBackButton);
+                    instructionBackPointer.SetActive(true);
+                } 
+                else 
+                {
+                EventSystem.current.SetSelectedGameObject(startButton);
+                startPointer.SetActive(true);
+                }
+            }    
         }
-        
     }
+
     public void OpenInstructionMenu() {
         instructionMenu.SetActive(true);
         EventSystem.current.SetSelectedGameObject(null);
