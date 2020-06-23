62db9afe07f63f680fc6df9304e0920bb8d5dcb9
Ting Xiao <tigerting98@gmail.com>
2020-06-23 16:54:26 +0800

    Change dialogue text
    Change the character dialogue for stage 1.
    Fix bugs regarding stage 1 boss explosion animation

--- Assets/scripts/LevelDescription.cs
+++ Assets/scripts/LevelDescription.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 [CreateAssetMenu(menuName = "Level Description")]
 public class LevelDescription : ScriptableObject
 {
+    [TextArea(3, 5)]
     public string levelDescription;
     public string nextLevelSceneString;
     public Sprite backgroundImage;
