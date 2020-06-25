318de30cbccda9ff0a8e4e2dec00c678407059db
Ting Xiao <tigerting98@gmail.com>
2020-06-26 00:05:09 +0800

    Create 2nd half of Stage 3
    Put together previously created Wave3 and Wave4 to make the complete level.

---
+++ Assets/waves/stage3/wave3/WavePattern4.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+struct WavePattern4Subwave 
+{
+    public float spawnDelay;
+    public int numberOfEnemies;
+    public float startY;
+    public float midY;
+    public float endY;
+    public int lines;
+    
+}
+public class WavePattern4 : EnemyWave
+{
+    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] float delay;
+    [SerializeField] int number = 5;
+    [SerializeField] float spawnRate = 0.5f;
+    [SerializeField] EnemyStats stats;
+    [SerializeField] float shootingDelay = 0.3f;
+    [SerializeField] float bulletSpeed = 3f;
+    [SerializeField] float shotAngle = 20f;
+    [SerializeField] int bulletsPerLine = 10;
+    [SerializeField] float bulletDamage = 10f;
+    [SerializeField] List<WavePattern4Subwave> subwaveList;
+    protected Enemy enemy;
+    protected Bullet bullet;
+
+    public virtual void SetUp()
+    {
+
+    }
+    public override void SpawnWave()
+    {
+        SetUp();
+        foreach (WavePattern4Subwave subwave in subwaveList) 
+        {
+            StartCoroutine(SpawnSubwave(subwave.spawnDelay, subwave.numberOfEnemies, subwave.startY, subwave.midY, subwave.endY, subwave.lines));
+        }
+    }
+
+    IEnumerator SpawnSubwave(float spawnDelay, int numberOfEnemies, float startY, float midY, float endY, int lines) 
+    {
+        Enemy enemy = this.enemy;
+        yield return new WaitForSeconds(spawnDelay);
+        Functions.StartMultipleCustomCoroutines(this, i => SpawnEnemy(moveSpeed, 0, new Vector2(-4.1f, startY), new Vector2(0f, midY), new Vector2(-4.1f, endY), lines, enemy), numberOfEnemies, spawnRate);
+        Functions.StartMultipleCustomCoroutines(this, i => SpawnEnemy(moveSpeed, 0, new Vector2(4.1f, startY), new Vector2(0f, midY), new Vector2(4.1f, endY), lines, enemy), numberOfEnemies, spawnRate);
+    }
+
+    IEnumerator SpawnEnemy(float speed, float firingDelay, Vector2 start, Vector2 mid, Vector2 end, int lines, Enemy enemy)
+    {
+
+        Enemy en = Instantiate(enemy, start, Quaternion.identity);
+        en.SetEnemy(stats, false);
+        
+        float timeToMid = en.movement.MoveTo(mid, speed);
+
+        yield return new WaitForSeconds(shootingDelay);
+
+        if(en) {
+            float bulletSpeed = this.bulletSpeed;
+            float angle = Patterns.AimAt(en.transform.position, GameManager.playerPosition);
+            for (int i = 0; i < bulletsPerLine; i++) {
+                Patterns.ShootMultipleStraightBullet(GameManager.gameData.leafBullet1, bulletDamage, en.transform.position, bulletSpeed, angle, shotAngle, lines);
+                bulletSpeed += 0.5f; 
+            }
+        } 
+
+        yield return new WaitForSeconds(timeToMid - shootingDelay);
+
+        if (en) 
+        {
+            float timeToEnd = en.movement.MoveTo(end, speed);
+    
+            yield return new WaitForSeconds(timeToEnd);
+
+            if(en)
+            {
+            Destroy(en.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/waves/stage3/wave4/WavePattern5.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+struct WavePattern5Subwave
+{
+    public float spawnDelay;
+    public float startX;
+    public float endY;
+    public bool summonBoss;
+
+}
+public class WavePattern5 : EnemyWave
+{
+    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] EnemyStats stats;
+    [SerializeField] float bulletSpeed = 5f;
+    [SerializeField] float shotRate = 0.5f;
+    [SerializeField] float shotAngle = 20f;
+    [SerializeField] int numberOfRings = 3;
+    [SerializeField] int bulletsPerRing = 10;
+    [SerializeField] float bulletDamage = 10f;
+    [SerializeField] float stationaryTime = 1f;
+    [SerializeField] List<WavePattern5Subwave> subwaveList;
+    protected Enemy enemy;
+    protected Bullet bullet;
+
+    public virtual void SetUp()
+    {
+
+    }
+
+    public override void SpawnWave()
+    {
+        SetUp();
+        foreach (WavePattern5Subwave subwave in subwaveList) 
+        {
+            StartCoroutine(SpawnSubwave(subwave.spawnDelay, subwave.startX, subwave.endY, subwave.summonBoss));
+        }
+    }
+
+    IEnumerator SpawnSubwave(float spawnDelay, float startX, float endY, bool summonBoss) 
+    {
+        Enemy enemy = this.enemy;
+        yield return new WaitForSeconds(spawnDelay);
+        StartCoroutine(SpawnEnemy(moveSpeed, new Vector2(startX, 4.2f), new Vector2(startX, endY)));
+        StartCoroutine(SpawnEnemy(moveSpeed, new Vector2(-startX, 4.2f), new Vector2(-startX, endY)));
+        if (summonBoss)
+        {
+            yield return new WaitForSeconds(10f);
+            GameManager.DestoryAllEnemyBullets();
+            GameManager.DestroyAllNonBossEnemy(false);
+            GameManager.SummonEndBoss();
+        }
+    }
+
+    IEnumerator SpawnEnemy(float speed, Vector2 start, Vector2 end)
+    {
+
+        Enemy en = Instantiate(enemy, start, Quaternion.identity);
+        en.SetEnemy(stats, false);
+        
+        float timeToEnd = en.movement.MoveTo(end, speed);
+        
+        yield return new WaitForSeconds(timeToEnd);
+
+        if (en)
+        {
+            en.shooting.StartCoroutine(Functions.RepeatCustomActionXTimes(i => Patterns.RingOfBullets(bullet, bulletDamage, en.transform.position, bulletsPerRing, 0, bulletSpeed), shotRate, numberOfRings));
+        }
+
+        yield return new WaitForSeconds(stationaryTime);
+
+        if(en) 
+        {   
+            float timeToReturn = en.movement.MoveTo(start, speed);
+            yield return new WaitForSeconds(timeToReturn);
+        }
+
+        if (en)
+        {
+            Destroy(en.gameObject);
+        }
+    }
+}
