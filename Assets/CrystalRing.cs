4579acb6919eec6502177a866719e0f85fad2b72
Aloysius Lim <aloysiuslim.dw@gmail.com>
2020-08-17 00:25:13 +0800

    beautify main menu and change stage5 endboss sprite

---
+++ Assets/CrystalRing.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalRing : MonoBehaviour
+{
+    [SerializeField] List<Crystal> crystals;
+    [SerializeField] float angularvel;
+    [SerializeField] int numberOfCrystals;
+    [SerializeField] float maxX, maxY;
+    [SerializeField] float angularvelup, amp;
+    float startpos;
+    float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numberOfCrystals; i++) {
+            float startangle = 2 * Mathf.PI / numberOfCrystals * i;
+            Crystal crystal = Instantiate(crystals[i%3], this.transform.position+new Vector3(maxX * Mathf.Cos(startangle),
+            maxY * Mathf.Sin(startangle), 0.5f * Mathf.Sin( startangle)), Quaternion.identity, this.transform);
+            crystal.startangle = startangle;
+            crystal.angularvel = angularvel;
+            crystal.maxx = maxX;
+            crystal.maxy = maxY;
+            crystal.maxz = 0.5f;
+        }
+        startpos = this.transform.position.y;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(this.transform.position.x, startpos + amp* Mathf.Sin(angularvelup*time), this.transform.position.z);
+        time += Time.deltaTime;
+    }
+}
---
+++ Assets/scripts/Crystal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crystal : MonoBehaviour
+{
+    float time = 0;
+    public float angularvel;
+    public float maxy, maxx, maxz;
+    public float startangle;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = new Vector3(maxx * Mathf.Cos(angularvel * time + startangle),
+            maxy * Mathf.Sin(angularvel * time + startangle), maxz * Mathf.Sin(angularvel * time + startangle));
+        time += Time.deltaTime;
+    }
+}
